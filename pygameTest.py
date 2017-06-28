7787e806deb0e143d016f22902409e2772d29250
Flye001 <philip.org1@gmail.com>
2017-06-28 18:53:08 +0100

    experimenting

--- pygameTest.py
+++ pygameTest.py
@@ -1,12 +1,20 @@
 import time
 import pygame
 
+#usrtext = raw_input("Do some shit: ")
+
 pygame.init()
 screen = pygame.display.set_mode((800,480))
 
 screen.fill((0,255,0))
+
+pygame.font.init()
+myfont= pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render("Welcome to my Pokedex", False, (0, 0, 0))
+screen.blit(textsurface,(20,20))
+#screen.fill((0,255,0))
 #pygame.Surface.fill((2555,0,0))
 
-pygame.display.flip()
-time.sleep(5)
+pygame.display.update()
+time.sleep(15)
 pygame.quit()
