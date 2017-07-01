8569680a1e5298f4662afb657576220390648c2e
Flye001 <philip.org1@gmail.com>
2017-07-01 11:20:26 +0100

    fixed tabs

--- code/mainCode.py
+++ code/mainCode.py
@@ -6,14 +6,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,480))
 
 def startScreen():
-  startScreen = '/home/pi/PokedexV2/pics/start-screen.png'
-  startScreenPNG = pygame.image.load(startScreen).convert_alpha()
-  screen.blit(startScreenPNG, (0,0))
-  pygame.display.update()
-  
-  while True:
-	  for event in pygame.event.get():
-		  if event.type == pygame.MOUSEBUTTONDOWN:
-			  sys.exit()
+	startScreen = '/home/pi/PokedexV2/pics/start-screen.png'
+	startScreenPNG = pygame.image.load(startScreen).convert_alpha()
+	screen.blit(startScreenPNG, (0,0))
+	pygame.display.update()
+	
+	while True:
+		for event in pygame.event.get():
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			sys.exit()
 
 startScreen()
