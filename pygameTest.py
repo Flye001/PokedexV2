c8d35a3f576b0b04ee7016e5282c0a2f016c0fb4
Flye001 <philip.org1@gmail.com>
2017-07-01 10:47:54 +0100

    testing stuff

--- pygameTest.py
+++ pygameTest.py
@@ -9,14 +9,14 @@ screen = pygame.display.set_mode((800,480))
 
 screen.fill((0,255,0))
 
-psyduck = '/home/pi/PokedexV2/pics/psyduck.png'
-psyduckPNG = pygame.image.load(psyduck).convert_alpha()
-screen.blit(psyduckPNG, (0,0))
+startScreen = '/home/pi/PokedexV2/pics/start-screen.png'
+startScreenPNG = pygame.image.load(startScreen).convert_alpha()
+screen.blit(startScreenPNG, (0,0))
 
 pygame.font.init()
-myfont= pygame.font.SysFont('Comic Sans MS', 30)
-textsurface = myfont.render("Welcome to my Pokedex", False, (0, 0, 0))
-screen.blit(textsurface,(20,20))
+#myfont= pygame.font.SysFont('Comic Sans MS', 30)
+#textsurface = myfont.render("Welcome to my Pokedex", False, (0, 0, 0))
+#screen.blit(textsurface,(20,20))
 #screen.fill((0,255,0))
 #pygame.Surface.fill((2555,0,0))
 
