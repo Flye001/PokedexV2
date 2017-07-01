a44df10ba99bf9672739b567e78a2061e55fe500
Flye001 <philip.org1@gmail.com>
2017-07-01 10:42:53 +0100

    testing stuff

--- pygameTest.py
+++ pygameTest.py
@@ -1,5 +1,6 @@
 import time
 import pygame
+import sys
 
 #usrtext = raw_input("Do some shit: ")
 
@@ -7,6 +8,10 @@ pygame.init()
 screen = pygame.display.set_mode((800,480))
 
 screen.fill((0,255,0))
+
+psyduck = '/home/pi/PokedexV2/pics/psyduck.png'
+psyduckPNG = pygame.image.load(psyduck).convert_alpha()
+screen.blit(psyduckPNG, (0,0))
 
 pygame.font.init()
 myfont= pygame.font.SysFont('Comic Sans MS', 30)
@@ -16,5 +21,11 @@ screen.blit(textsurface,(20,20))
 #pygame.Surface.fill((2555,0,0))
 
 pygame.display.update()
-time.sleep(15)
-pygame.quit()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			sys.exit()
+
+#time.sleep(15)
+#pygame.quit()
