ac074a47815b2d17c6f862795180f454968b5274
Flye001 <philip.org1@gmail.com>
2017-07-01 11:52:40 +0100

    changing code

--- code/mainCode.py
+++ code/mainCode.py
@@ -2,18 +2,22 @@ import time
 import pygame
 import sys
 
+pics = '/home/pi/PokedexV2/pics/'
+
 pygame.init()
 screen = pygame.display.set_mode((800,480))
 
 def startScreen():
-	startScreen = '/home/pi/PokedexV2/pics/start-screen.png'
+	startScreen = pics+'startScreen.png'
 	startScreenPNG = pygame.image.load(startScreen).convert_alpha()
 	screen.blit(startScreenPNG, (0,0))
 	pygame.display.update()
-	
 	while True:
 		for event in pygame.event.get():
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			sys.exit()
+
+def mainMenu():
+	background = pics+'background.png'
 
 startScreen()
