6efcec4d165fc72e3406827ae72197d910c54818
Flye001 <philip.org1@gmail.com>
2017-07-01 14:28:55 +0100

    made menu buttons clickable

--- code/mainCode.py
+++ code/mainCode.py
@@ -25,15 +25,44 @@ def mainMenu():
 	backgroundPNG = pygame.image.load(background).convert_alpha()
 	start = pics+'start.png'
 	startPNG = pygame.image.load(start).convert_alpha()
-	credits = pics+'credits.png'
-	creditsPNG = pygame.image.load(credits).convert_alpha()
+	credit = pics+'credits.png'
+	creditPNG = pygame.image.load(credit).convert_alpha()
 	exit = pics+'exit.png'
 	exitPNG = pygame.image.load(exit).convert_alpha()
+	#Rect some stuff
+	startPos = pygame.Rect(60, 125, 268, 71)
+	creditPos = pygame.Rect(60, 246, 268, 71)
+	exitPos = pygame.Rect(60, 367, 268, 71)
 	#blit images
 	screen.blit(backgroundPNG, (0,0))
-	screen.blit(startPNG, (60,125))
-	screen.blit(creditsPNG, (60,246))	#add 121
-	screen.blit(exitPNG, (60,367))
+	screen.blit(startPNG, startPos)
+	screen.blit(creditPNG, creditPos)	#image size 268x71 so add 121
+	screen.blit(exitPNG, exitPos)
+	#display
 	pygame.display.update()
+	#move Rects
+	startPos = startPNG.get_rect()
+	startPos = startPos.move(60, 125)
+	creditPos = creditPNG.get_rect()
+	creditPos = creditPos.move(60, 246)
+	exitPos = exitPNG.get_rect()
+	exitPos = exitPos.move (60, 367)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				mouse_pos = event.pos
+				#print mouse_pos
+				if startPos.collidepoint(mouse_pos):
+					print('Starting...')
+					sys.exit()
+				if creditPos.collidepoint(mouse_pos):
+					print('Credits:')
+					sys.exit()
+				if exitPos.collidepoint(mouse_pos):
+					print('bye')
+					sys.exit()
 	
 startScreen()
