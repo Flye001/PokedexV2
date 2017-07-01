ef94d3d66f8466be3cb0f44bea0a3027fc42a30c
Flye001 <philip.org1@gmail.com>
2017-07-01 12:35:08 +0100

    added menu buttons

--- code/mainCode.py
+++ code/mainCode.py
@@ -20,9 +20,20 @@ def startScreen():
 
 def mainMenu():
 	screen.fill(black)
+	#define pics
 	background = pics+'background.png'
 	backgroundPNG = pygame.image.load(background).convert_alpha()
+	start = pics+'start.png'
+	startPNG = pygame.image.load(start).convert_alpha()
+	credits = pics+'credits.png'
+	creditsPNG = pygame.image.load(credits).convert_alpha()
+	exit = pics+'exit.png'
+	exitPNG = pygame.image.load(exit).convert_alpha()
+	#blit images
 	screen.blit(backgroundPNG, (0,0))
+	screen.blit(startPNG, (60,125))
+	screen.blit(creditsPNG, (60,246))	#add 121
+	screen.blit(exitPNG, (60,367))
 	pygame.display.update()
 	
 startScreen()
