d9d0ac4978a8bcbcc02c0e52e8dd403eb3999851
Flye001 <philip.org1@gmail.com>
2017-07-01 12:18:51 +0100

    added main menu image

--- code/mainCode.py
+++ code/mainCode.py
@@ -3,21 +3,26 @@ import pygame
 import sys
 
 pics = '/home/pi/PokedexV2/pics/'
-
+black = (0,0,0)
 pygame.init()
 screen = pygame.display.set_mode((800,480))
 
 def startScreen():
+	screen.fill(black)
 	startScreen = pics+'start-screen.png'
 	startScreenPNG = pygame.image.load(startScreen).convert_alpha()
 	screen.blit(startScreenPNG, (0,0))
 	pygame.display.update()
 	while True:
 		for event in pygame.event.get():
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			sys.exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				mainMenu()
 
 def mainMenu():
+	screen.fill(black)
 	background = pics+'background.png'
-
+	backgroundPNG = pygame.image.load(background).convert_alpha()
+	screen.blit(backgroundPNG, (0,0))
+	pygame.display.update()
+	
 startScreen()
